b33828d15135cb391f971696ddb32c6d331ee2a2
KH036s <KH036s@KH036-16.uncanet.unca.edu>
2017-11-17 14:18:47 -0500

    Changed up with question creation to reduce dup code

--- src/edu/unca/csci201/QuizTime.java
+++ src/edu/unca/csci201/QuizTime.java
@@ -3,19 +3,20 @@ package edu.unca.csci201;
 public class QuizTime {
 
 	public static void main(String[] args) {
-		
-		
-	MultipleChoiceQuestion q1 = new MultipleChoiceQuestion();
-	Question q2 = new TrueFalseQuestion();
-	Question q3 = new ShortAnswerQuestion();
+	
+		MultipleChoiceQuestion MCQVec []= new MultipleChoiceQuestion[10];
+		TrueFalseQuestion TFVec[] = new TrueFalseQuestion[10];
+		ShortAnswerQuestion SAVec[] = new ShortAnswerQuestion[5];
+	//MultipleChoiceQuestionVec[0] = new MultipleChoiceQuestion();
+	
 	Quiz hurray = new Quiz();
 	
-	q1.setQuestion("What is the Meaning of the univers \n a)2 \n b)4 \n c)24 \n d)42");
-	q1.setAnswer("d");
+	MCQVec[0].setQuestion("What is the Meaning of the univers \n a)2 \n b)4 \n c)24 \n d)42");
+	MCQVec[0].setAnswer("d");
+	TFVec[0].setQuestion("The best wat to travel is by Blue Police box?  \n True False");
+	TFVec[0].setAnswer("true");
+	// need to set up an array probly three of them so that it is easer to iterate to add the question
 	
-	
-	
-	hurray.addQuestion(q1);
 	System.out.println(hurray.giveQuiz());
 	
 
--- src/edu/unca/csci201/TrueFalseQuestion.java
+++ src/edu/unca/csci201/TrueFalseQuestion.java
@@ -2,13 +2,9 @@ package edu.unca.csci201;
 
 public class TrueFalseQuestion extends SuperQuestion implements Question {
 
-	@Override
-	public String getQuestion() {
-		// TODO Auto-generated method stub
-		return question;
-	}
+	
 	public void setAnswer(String sanswer) {
-		if (sanswer!="true"||sanswer!="false") {
+		if (sanswer.equalsIgnoreCase("true")||sanswer.equalsIgnoreCase("false")){
 			System.out.println("error!! wrong question format");
 		}
 		else {
